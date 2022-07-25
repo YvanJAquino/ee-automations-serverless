335da182e3de60a5d72a2f721ea7401e23e0641d
Yvan J. Aquino <yvan.aquino@cloud-colosseum.net>
2022-07-25 19:08:38 +0000

    fixed typos in 2-orgpolicy.tf

--- 2-orgpolicy.tf
+++ 2-orgpolicy.tf
@@ -2,7 +2,7 @@
 # Allow Domain Restricted Sharing
 resource "google_project_organization_policy" "domain_restricted_sharing" {
   constraint = "iam.allowedPolicyMemberDomains"
-  project    = local.Project
+  project    = local.project
 
   list_policy {
     allow {
@@ -15,7 +15,7 @@ resource "google_project_organization_policy" "domain_restricted_sharing" {
 # Allow Ingress settings for Cloud Functions
 resource "google_project_organization_policy" "functions_allowed_ingress_settings" {
   constraint = "cloudfunctions.allowedIngressSettings"
-  project    = local.Project
+  project    = local.project
 
   list_policy {
     allow {
@@ -28,7 +28,7 @@ resource "google_project_organization_policy" "functions_allowed_ingress_setting
 # Allow Ingress settings for Cloud Run
 resource "google_project_organization_policy" "run_allowed_ingress_settings" {
   constraint = "run.allowedIngress"
-  project    = local.Project
+  project    = local.project
 
   list_policy {
     allow {
